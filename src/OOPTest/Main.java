b2ab89b3cab9afaa4f959139abf22052dbad15ce
Glen Roberts <glensroberts@gmail.com>
2015-12-02 22:52:08 -0500

    I made the burger ordering process have a lil style. To the ladies out there .. ordering these burgers brings your masculinity out so own it. Don't have a beard? Grow one it'll be worth it.

--- src/OOPTest/Main.java
+++ src/OOPTest/Main.java
@@ -9,6 +9,7 @@ import java.text.NumberFormat;
 public class Main {
     public static void main(String[] args) {
         Burger orderOne = new Burger("roll","beef",BigDecimal.valueOf(2.50));
+        System.out.println("Hey, you know exactly what you want. Take your time building your masterpiece.");
         orderOne.addLettuce();
         orderOne.addCarrot();
         System.out.println("Your bread type is " + orderOne.getBreadType() + ", meat is " + orderOne.getMeat()
@@ -18,6 +19,7 @@ public class Main {
         System.out.println();
 
         HealthyBurger orderTwo = new HealthyBurger("turkey",BigDecimal.valueOf(4.00));
+        System.out.println("Hey, you're a Healthy kind of man. You want to be able to flex in the mirror and say \"Ohh Yea I'm a sexy beast\".");
         orderTwo.addCarrot();
         orderTwo.addAvocado();
         orderTwo.addSprouts();
@@ -28,6 +30,7 @@ public class Main {
         System.out.println();
 
         DeluxeBurger orderThree = new DeluxeBurger("sesame seed bun","angus beef",BigDecimal.valueOf(11.00),"Sprite","medium fries");
+        System.out.println("Hey, you're a Deluxe kind of man. You want to have delicious toppings dropping from your mouth to your beard for safe keeping later.");
         System.out.println("Your bread type is " + orderThree.getBreadType() + ", meat is " + orderThree.getMeat()
                 + " the base price is " + NumberFormat.getCurrencyInstance().format(orderThree.getBasePrice()) + " with your additions being "
                 + orderThree.getDrink() + " and " + orderThree.getFries() +  " and the total price is " + NumberFormat.getCurrencyInstance().format(orderThree.calculatePrice()));
